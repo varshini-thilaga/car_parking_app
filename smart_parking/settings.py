ac89c09c1729dafbac99b820431f0e263a4cbf44
Varshini Thilaga <varshinichellamuthu245@gmail.com>
2026-05-13 10:15:19 +0530

    fix: add [200~STATIC_ROOT and ALLOWED_HOSTS for Render deployment

--- smart_parking/settings.py
+++ smart_parking/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-bd1b+f7khkjlca0cs&i+cp+ej0qadd98f!5$yzprfd0p76s!ak
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -115,8 +115,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [BASE_DIR / 'static'] if (BASE_DIR / 'static').exists() else []
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
